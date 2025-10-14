84217ff0082edce66bd2b4c5aa2514fad0c1c241
estafa <whalefancypants@gmail.com>
2025-10-13 20:08:49 -0400

    First add

---
+++ pits.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+struct Board {
+    int south [6]; 
+    int north[6];
+};
+
+int main() {
+    
+    char ch;
+
+
+
+
+    printf("  12   11   10    9    8    7\n");
+    printf("+----+----+----+----+----+----+\n");
+
+
+
+
+    printf("+----+----+----+----+----+----+\n");
+    printf("   1    2    3    4    5    6\n");
+    return 0;
+
+}
+
+/* add one to each array */
